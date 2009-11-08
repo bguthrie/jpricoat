7d6fd5c4d957a1043e5f3487d63a1ab63d874b7e
Brian Guthrie <btguthrie@gmail.com>
2009-11-07 18:55:28 -0800

    Removed needless import from XPathConverterTest.

--- test/css2xpath/XPathConverterTest.java
+++ test/css2xpath/XPathConverterTest.java
@@ -3,7 +3,6 @@ package css2xpath;
 import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import com.steadystate.css.*;
 import com.steadystate.css.parser.*;
 
 public class XPathConverterTest {
